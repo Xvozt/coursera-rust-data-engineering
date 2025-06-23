a9f5a2289d3e65137ace67ee339b0a09eb744989
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-06-23 23:01:03 +0400

    module Sequencies, maps usage

--- maps/src/main.rs
+++ maps/src/main.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+
+fn frequencies(numbers: Vec<i32>) -> Vec<(i32, u32)> {
+    let mut frequencies: HashMap<i32, u32> = HashMap::new();
+
+    for num in numbers {
+        let frequency = frequencies.entry(num).or_insert(0);
+        *frequency += 1;
+    }
+
+    let mut result = Vec::new();
+    
+    for (num, frequency) in frequencies {
+        result.push((num, frequency));
+    }
+
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let example = vec![1,2,3,4,5,6,7,8,-5,3,4,-5];
+
+    let frequencies = frequencies(example);
+
+    println!("Frequency of each number in the example is: {:?}", frequencies);
 }
