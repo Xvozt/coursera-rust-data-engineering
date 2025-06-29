bdce188c5cb3b5bc4a960289f58a4cf656ea928a
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-06-29 21:54:56 +0400

    Fruit salad creator in a form of cli. Pass number of fruits and your own to get a shuffled fruit salad

---
+++ fruit-salad-cli/src/lib.rs
@@ -0,0 +1,37 @@
+use rand::seq::SliceRandom;
+use rand::rng;
+
+pub static DEFAULT_FRUITS: &[&str] = &[
+    "Arbutus",
+    "Loquat", 
+    "Strawberry Tree Berry",
+    "Pomegranate",
+    "Fig",
+    "Cherry",
+    "Orange",
+    "Pear",
+    "Peach",
+    "Apple",
+];
+
+pub fn create_fruit_salad(num_fruits: usize) -> Vec<String> 
+{
+    let mut fruits: Vec<String> = DEFAULT_FRUITS.iter().map(|&s| s.to_string()).collect();
+    let mut rng = rng();
+    
+    fruits.shuffle(&mut rng);
+
+    fruits.into_iter().take(num_fruits).collect()
+}
+
+pub fn create_fruit_salad_with_user_fruits(num_fruits: usize, users_fruits: &mut Vec<String>) -> Vec<String> 
+{
+    let mut fruits: Vec<String> = DEFAULT_FRUITS.iter().map(|&s| s.to_string()).collect();
+    fruits.append(users_fruits);
+    
+    let mut rng = rng();
+    fruits.shuffle(&mut rng);
+
+    fruits.into_iter().take(num_fruits).collect()
+
+}
---
+++ fruit-salad-cli/src/main.rs
@@ -0,0 +1,45 @@
+use clap::Parser;
+use fruit_salad_cli::{create_fruit_salad, create_fruit_salad_with_user_fruits};
+
+#[derive(Parser)]
+#[clap(
+    version = "1.0",
+    author = "Your Name <your.email@example.com>",
+    about = "Number of fruits to include in the salad"
+)]
+struct Opts {
+    #[clap(short, long)]
+    number: usize,
+    #[clap(short, long, num_args = 1..)]
+    mine: Option<Vec<String>>
+}
+
+fn main() {
+    let opts: Opts = Opts::parse();
+
+    // Get the number of fruits the user requested
+    let num_fruits = opts.number;
+
+    match opts.mine {
+        Some(mut user_fruits) => {
+            let mut salad = create_fruit_salad_with_user_fruits(num_fruits, &mut user_fruits);
+            salad.sort();
+            println!(
+                "Created Fruit salad with {} fruits: {:?}",
+                salad.len(),
+                salad
+            );
+        },
+        None => {
+            let mut salad = create_fruit_salad(num_fruits);
+
+            salad.sort();
+
+             println!(
+                "Created fruit salad with {} fruits: {:?}",
+                salad.len(),
+                salad
+            );
+        }
+    }
+}
