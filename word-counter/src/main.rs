e9e6d8701a319bf6fc9cec6d2e6225924391ea39
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-06-30 20:36:57 +0400

    Sort the result of counting words based on frequency

--- word-counter/src/main.rs
+++ word-counter/src/main.rs
@@ -2,10 +2,16 @@ use word_counter::count_words;
 fn main() {
     
     if let Ok(count) = count_words("example.txt") {
-        for (word, count) in &count {
+        // for (word, count) in &count {
+        //     println!("{word}: {count}");
+        let mut word_counts: Vec<(String, u32)> = count.into_iter().collect();
+        word_counts.sort_by(|a, b| b.1.cmp(&a.1));
+        
+        for (word, count) in &word_counts {
             println!("{word}: {count}");
         }
-    } else {
+
+        } else {
         println!("Error happened!")
     }
 
