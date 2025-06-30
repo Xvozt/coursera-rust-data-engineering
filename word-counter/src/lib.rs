3e5bcb336f70f01ea9045d315eae415a0a7fe6a2
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-06-30 20:27:03 +0400

    Added buffer, made better processing with trimming

--- word-counter/src/lib.rs
+++ word-counter/src/lib.rs
@@ -1,20 +1,22 @@
 use std::{collections::HashMap};
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{prelude::*, BufReader};
 
 pub fn count_words(path: &str) -> std::io::Result<HashMap<String, u32>> {
-    let mut file = File::open(path)?;
-    
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    let word_list: Vec<&str> = contents.split(' ').collect();
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
 
     let mut frequency_map = HashMap::new();
 
-    for word in word_list {
-        *frequency_map.entry(word.to_string()).or_insert(0) += 1;
-    };
+    for line in reader.lines() {
+        let line = line?;
 
+        for word in line.split_whitespace() {
+            let word = word.trim_matches(|c: char| !c.is_alphanumeric()).to_lowercase();
+            if !word.is_empty() {
+                *frequency_map.entry(word).or_insert(0) += 1;
+            }
+        }
+    }
     Ok(frequency_map)
 }
